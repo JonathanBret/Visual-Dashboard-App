2abcc478033bad20f403144c43e72b9a6646f182
JonathanBret <bret.jonathan@outlook.fr>
2026-02-13 00:49:53 +0100

    add Bar chart to dashboard with tasks data

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import Card from "../components/Card.jsx"
 import { useQuery } from "@tanstack/react-query"
+import { Bar } from "react-chartjs-2"
 
 const fetchTasks = async () => {
     const res = await fetch("http://localhost:3000/tasks")
@@ -13,11 +14,20 @@ export default function Dashboard() {
     if (isLoading) return <div>"Chargement..."</div>
     if (isError) return <div>"Erreur lors du chargement</div>
 
+    const labels = ["done", "pending"]
+    const values = [1, 1]
+    const data = {
+        labels: labels,
+        datasets: [{ label: "Tasks", data: values, backgroundColor: ["green","red"] }]
+}
+
+
     return (
       <div className="p-4 text-2xl text-blue-500">
        {tasks.map(task => (
         <Card key={task.id} title={task.title} content={task.content} />
       ))}
+       <Bar data={data} />
       </div>
     )
   }
