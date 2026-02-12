24a4ae558ff2ca1fca6de50ada744217fd46cd85
JonathanBret <bret.jonathan@outlook.fr>
2026-02-12 23:48:18 +0100

    add react query and connect tasks API

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,17 +1,24 @@
+import { useQuery } from "@tanstack/react-query"
 import Card from "../components/Card.jsx"
 
-export default function Tasks() {
-    const tasks = [
-        { id:1, title: "Tâche 1", content: "Finir le projet"},
-        { id:2, title: "Tâche 2", content: "Envoyer mail"},
-        { id:3, title: "Tâche 3", content: "Préparer ca"},
-    ]
-    return (
-      <div className="p-6 text-2xl">
-        {tasks.map(task => (
-            <Card key={task.id} title={task.title} content={task.content} />
-        ))}
-      </div>
-    )
+
+const fetchTasks = async () => {
+    const res = await fetch("http://localhost:3000/tasks")
+    if (!res.ok) throw new Error("Erreur lors du fetch")
+    return res.json()
   }
   
+export default function Tasks() {
+  const { data: tasks, isLoading, isError } = useQuery(["tasks"], fetchTasks)
+
+  if (isLoading) return <div>Chargement...</div>
+  if (isError) return <div>Erreur lors du chargement</div>
+
+  return (
+    <div className="p-6 space-y-4">
+      {tasks.map(task => (
+        <Card key={task.id} title={task.title} content={task.content} />
+      ))}
+    </div>
+  )
+}
