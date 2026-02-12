3ab045f4ad117c8aa2c905d3c51c28e09c58ad42
JonathanBret <bret.jonathan@outlook.fr>
2026-02-13 00:05:32 +0100

    connect dashboard to tasks API

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,23 @@
+import Card from "../components/Card.jsx"
+import { useQuery } from "@tanstack/react-query"
+
+const fetchTasks = async () => {
+    const res = await fetch("http://localhost:3000/tasks")
+    if (!res.ok) throw new Error("Erreur lors du fetch")
+    return res.json()
+  }
+
 export default function Dashboard() {
+    const {data: tasks, isLoading, isError} = useQuery(["tasks"], fetchTasks)
+
+    if (isLoading) return <div>"Chargement..."</div>
+    if (isError) return <div>"Erreur lors du chargement</div>
+
     return (
       <div className="p-4 text-2xl text-blue-500">
-        Dashboard visible !
+       {tasks.map(task => (
+        <Card key={task.id} title={task.title} content={task.content} />
+      ))}
       </div>
     )
   }
